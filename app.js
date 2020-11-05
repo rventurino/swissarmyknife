43ece0935d339b95a8b8d21a5ac9a8891725563a
Ryan Venturino <ryan.venturino@gmail.com>
2020-11-05 13:06:39 -0500

    A

---
+++ app.js
@@ -0,0 +1,10 @@
+/*Triangle*/
+// Can also be included with a regular script tag
+import Typed from "typed.js";
+
+var options = {
+  strings: ["Loading..."],
+  typeSpeed: 40,
+};
+
+var typed = new Typed(".type", options);
